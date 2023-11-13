b048de8306ac83bc2396fbd62cc9b3cfe652f822
Luis Lucena <lucenaluis137@gmail.com>
2023-11-13 01:15:39 +0100

    :sparkles: Feat: add Aave interface

---
+++ contracts/interfaces/IAaveLendingPool.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IAaveLendingPool {
+    function deposit(
+        address asset,
+        uint256 amount,
+        address onBehalfOf,
+        uint16 referralCode
+    ) external;
+
+    function withdraw(address asset, uint256 amount, address to) external;
+
+    function getReserveData(
+        address asset
+    )
+        external
+        view
+        returns (
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint256,
+            uint40
+        );
+}
