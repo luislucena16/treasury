0b995f7142f3dab6232865276f4c01ebdc043925
Luis Lucena <lucenaluis137@gmail.com>
2023-11-13 04:46:10 +0100

    :sparkles: Feat: improve hardhat config structure

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,38 @@
+require("hardhat-contract-sizer");
+require("solidity-coverage");
+require("hardhat-gas-reporter");
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const MUMBAI_RPC_URL = process.env.MUMBAI_RPC_URL;
+const ETH_FORK_URL = process.env.ETH_FORK_URL;
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    version: "0.8.19",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  paths: { sources: "contracts", tests: "test" },
+  networks: {
+    hardhat: {
+      // url: "http://127.0.0.1:8545/",
+      forking: {
+        // note: an Ethereum fork is being used
+        url: ETH_FORK_URL,
+        blockNumber: 17971384,
+      },
+    },
+    mumbai: {
+      url: MUMBAI_RPC_URL,
+      blockNumber: 17971384,
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
